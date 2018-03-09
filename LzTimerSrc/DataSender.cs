ecd0dc1c3003f83a51d2185cbc32e6834824558d
Krzysztof Kot <krzykot@gmail.com>
2018-03-09 21:13:35 +0100

    sending json works

--- LzTimerSrc/DataSender.cs
+++ LzTimerSrc/DataSender.cs
@@ -1,14 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using log4net.Config;
+using Newtonsoft.Json;
 
 namespace kkot.LzTimer
 {
     class DataSender : TimeTableUpdateListener
     {
+        private static readonly log4net.ILog Log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         private static readonly HttpClient client = new HttpClient();
 
-        private static readonly string url = "localhost:8080/api/periods";
+        private static readonly string url = "http://localhost:8080/api/periods";
 
         private readonly TokenReceiver tokenReceiver;
 
@@ -17,25 +23,29 @@ namespace kkot.LzTimer
             this.tokenReceiver = tokenReceiver;
         }
 
-        public Dictionary<String, String> ConvertPeriod(ActivityPeriod period)
+        public Dictionary<string, string> ConvertPeriod(ActivityPeriod period)
         {
             return new Dictionary<string, string>
             {
-                { "beginTime" , period.Start.ToString("o")},
-                { "endTime" , period.Start.ToString("o")},
+                { "beginTime" , period.Start.ToUniversalTime().ToString("o")},
+                { "endTime" , period.Start.ToUniversalTime().ToString("o")},
                 { "active", period.IsActive().ToString() }
             };
         }
 
-        public void Updated()
+        public async void Updated()
         {
             if (tokenReceiver.Token == null)
             {
                 return;
             }
-
-
-
+            var obj = JsonConvert.SerializeObject(
+                    ConvertPeriod(new ActivePeriod(DateTime.Now, DateTime.Now.AddSeconds(10))));
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", tokenReceiver.Token);
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+            var content = new StringContent(obj, Encoding.UTF8, "application/json");
+            var resonse = await client.PostAsync(url, content);
+            Log.Info(resonse);
         }
     }
 }
